a701547a024b87a835a78dd1eeb614fa6ade287d
yoshiaki <ikimono.miwa589@gmail.com>
2015-06-21 12:23:12 +0900

    リバーシに関する処理をReversiクラスに移管

---
+++ Reversi.rb
@@ -0,0 +1,60 @@
+#coding: utf-8
+require 'bundler'
+Bundler.require
+
+require 'sdl'
+
+class Reversi
+	def initialize(windowWidth,windowHeight,windowSpace,boardGridNum,stoneState,screen)
+		@windowSpace=windowSpace
+		@boardSize=windowWidth>windowHeight ? windowHeight-windowSpace*2 : windowWidth-windowSpace*2
+		@boardGridNum=boardGridNum
+		#マス目の状態格納用２次元配列(0:なし1:白2:黒)
+		@gridState=Array.new(boardGridNum) do
+			Array.new(boardGridNum,0)
+		end
+		@screen=screen
+		@stoneState=stoneState
+	end
+	
+	#盤面を描画
+	# @return [nil] 戻り地なし
+	def drawGrid()
+		@gridSize=@boardSize/@boardGridNum#1グリッドのサイズ
+		@endPlace=@boardSize+@windowSpace#縦横の線描画する終点座標
+		@lineColor=[0,0,255]#線の色
+
+		#横ラインを描画	
+		(@boardGridNum+1).times do |i|
+			y=@windowSpace+@gridSize*i
+			@screen.draw_line(@windowSpace,y,@endPlace,y,@lineColor)
+		end
+		#縦ラインを描画
+		(@boardGridNum+1).times do |i|
+			x=@windowSpace+@gridSize*i
+			@screen.draw_line(x,@windowSpace,x,@endPlace,@lineColor)
+		end
+	end
+
+	def drawStone()
+		xy=[@windowSpace+@gridSize/2,@windowSpace+@gridSize/2]
+		@color=[[0,255,255],[255,255,255],[0,0,0]]
+		@boardGridNum.times do |i|
+			@boardGridNum.times do |j|
+				@screen.draw_circle(xy[0],xy[1],@gridSize/2-5,@color[@gridState[i][j]],true)
+				xy[0]+=@gridSize;
+			end
+			xy[0]=@windowSpace+@gridSize/2
+			xy[1]+=@gridSize
+		end
+	end
+	
+	def addStone(x,y,state)
+		@gridState[y][x]=@stoneState[state]
+	end
+	
+	def getBoardGridSize()
+		return @gridSize
+	end
+end
+
--- main.rb
+++ main.rb
@@ -4,31 +4,30 @@ Bundler.require
 
 require 'sdl'
 
+require './Reversi'
+
 def main()
 	windowWidth=500
 	windowHeight=420
 	windowName='オセロ'#メインウィンドウの表示名
 	windowSpace=10#メインウィンドウの余白
 	boardGridNum=8#ボードの縦横のマス数
-	gridState=Array.new(boardGridNum) do Array.new(boardGridNum,0) end#マス目の状態格納用２次元配列(0:なし1:白2:黒)
 	stoneState={'none'=>0,'white'=>1,'black'=>2}#マス目の状態を表す値を持つハッシュ
-	initStonePlace=[[3,3,'white'],[3,4,'black'],[4,3,'black'],[4,4,'white']]
-
 	SDL.init(SDL::INIT_VIDEO)
 	screen = SDL::Screen.open(windowWidth,windowHeight, 16, SDL::SWSURFACE)
 	SDL::WM::set_caption(windowName,'testsprite.rb icon')
 
 	#背景色を赤で塗りつぶし
 	screen.fill_rect(0,0,windowWidth,windowHeight,[0,255,255])
-	#ウィンドウサイズで縦横のどちらか短い方を盤面の縦横サイズに
-	boardSize=windowWidth>windowHeight ? windowHeight-windowSpace*2 : windowWidth-windowSpace*2
+	
+	reversi=Reversi.new(windowWidth,windowHeight,windowSpace,boardGridNum,stoneState,screen)
 	#盤面を描画
-	gridSize=drawGrid(windowSpace,boardGridNum,boardSize,screen)
+	reversi.drawGrid()
 	#初期石を配置	
-	gridState[3][3]=stoneState['white']
-	gridState[3][4]=stoneState['black']
-	gridState[4][3]=stoneState['black']
-	gridState[4][4]=stoneState['white']
+	reversi.addStone(3,3,'white')
+	reversi.addStone(3,4,'black')
+	reversi.addStone(4,3,'black')
+	reversi.addStone(4,4,'white')
 
 	loop do
 	  while event = SDL::Event.poll
@@ -40,49 +39,12 @@ def main()
 			puts "x("+x.to_s+')'+' y('+y.to_s+')'
 			x-=windowSpace
 			y-=windowSpace
-			gridState[y/gridSize][x/gridSize]=stoneState['black']
+			gridSize=reversi.getBoardGridSize()
+			reversi.addStone(x/gridSize,y/gridSize,'black')
 		end
-		drawStone(gridState,gridSize,windowSpace,boardGridNum,screen)
+		reversi.drawStone()
 	  end
 	  screen.update_rect(0, 0, 0, 0)
-	end
-end
-
-#盤面を描画
-# @param [int] space ウィンドウの余白サイズ
-# @param [int] gridNum 縦横のグリッド数
-# @param [int] boardSize 縦横のボードの長さ
-# @param [SDL::Screen] screen 
-# @return [nil] 戻り地なし
-def drawGrid(space,gridNum,boardSize,screen)
-	gridSize=boardSize/gridNum#1グリッドのサイズ
-	endPlace=boardSize+space#縦横の線描画する終点座標
-	lineColor=[0,0,255]#線の色
-
-	#横ラインを描画	
-	(gridNum+1).times do |i|
-		y=space+gridSize*i
-		screen.draw_line(space,y,endPlace,y,lineColor)
-	end
-	#縦ラインを描画
-	(gridNum+1).times do |i|
-		x=space+gridSize*i
-		screen.draw_line(x,space,x,endPlace,lineColor)
-	end
-	
-	return gridSize
-end
-
-def drawStone(gridState,gridSize,space,gridNum,screen)
-	initxy=[space+gridSize/2,space+gridSize/2]
-	color=[[0,255,255],[255,255,255],[0,0,0]]
-	gridNum.times do |i|
-		gridNum.times do |j|
-			screen.draw_circle(initxy[0],initxy[1],gridSize/2-5,color[gridState[i][j]],true)
-			initxy[0]+=gridSize;
-		end
-		initxy[0]=space+gridSize/2
-		initxy[1]+=gridSize
 	end
 end
 
